048b80585c8d1aba0d4f713e419bb4b312737bf7
TheDakDakDak <thedakdakdak@gmail.com>
2025-07-13 14:26:25 -0700

    Testing frontend-backend connection

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.send('Workout Tracker Backend is running!');
 });
 
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Frontend and backend are connected!' });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
