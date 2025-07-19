530a6b71f40e5cffacd0ffeefdf5ac06416f0953
TheDakDakDak <thedakdakdak@gmail.com>
2025-07-19 10:31:35 -0700

    Broken link fix on landing page

--- public/index.js
+++ public/index.js
@@ -4,6 +4,6 @@ fetch('/api/session', {
 	.then(res => res.json())
 	.then(data => {
 		if(data.authenticated) {
-			window.location.href = "../WorkoutsHome/workoutsHome.html";
+			window.location.href = "/dashboard";
 		}
 	})
